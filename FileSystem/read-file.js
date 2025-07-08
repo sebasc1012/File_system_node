cfe0cf90af8f1f3e0d6eed05e44e924be12dc5d0
Sebastian Castaño <js.castano@globant.com>
2025-07-08 18:44:25 -0500

    Feat(add):read file

--- FileSystem/read-file.js
+++ FileSystem/read-file.js
@@ -1,7 +1,24 @@
 const fs = require("node:fs");
-const { Buffer } = require("buffer");
+const path = require("node:path");
+// const { Buffer } = require("buffer");
 
-const readFile = fs.readFileSync("./text.txt", "hex");
-console.log(readFile);
-const size = Buffer.poolSize;
-console.log(`Buffer pool size is ${size} bytes`);
+// const readFile = fs.readFileSync("./text.txt", "hex");
+// console.log(readFile);
+// const size = Buffer.poolSize;
+// console.log(`Buffer pool size is ${size} bytes`);
+
+fs.readFile(path.join(__dirname, "text.txt"), "utf-8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
+
+fs.writeFile(path.join(__dirname, "copy.txt"), "This is a copy", (err) => {
+  if (err) throw err;
+  console.log("it was succesfully created");
+});
+
+fs.appendFile();
+
+process.on("uncaughtException", (err) => {
+  console.error(`There was a uncaughterror: ${err}`);
+});
